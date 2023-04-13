6737eb819a6904d56e67d65e7de266cc0fca0290
The-Cyril5555 <cyrilbizouarn5@gmail.com>
2023-04-14 00:02:04 +0200

    Mise en place du responsive de la navbar

--- wiki/src/components/navbar/Navbar.jsx
+++ wiki/src/components/navbar/Navbar.jsx
@@ -1,27 +1,61 @@
-import React from 'react';
-import './Navbar.css';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
-const Navbar = () => {
+function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
   return (
-    <nav className="navbar">
-      <div className="navbar-brand">Wiki</div>
-      <div className="navbar-center">
-        <ul className="navbar-menu">
-          <li className="navbar-item">
-            <Link to="/" className="navbar-link">Accueil</Link>
-          </li>
-          <li className="navbar-item">
-            <Link to="/categorie" className="navbar-link">Catégorie</Link>
-          </li>
-        </ul>
-      </div>
-      <ul className="navbar-menu">
-        <li className="navbar-item">
-          <Link to="/connexion" className="navbar-link">Connexion</Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className="navbar">
+        <div className="nav-container">
+          <Link exact to="/" className="nav-logo">
+            WIKI 
+          </Link>
+
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link
+                exact
+                to="/"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Accueil
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                exact
+                to="/categorie"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Catégorie
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                exact
+                to="/connexion"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Connexion
+              </Link>
+            </li>
+      
+          </ul>
+          <div className="nav-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 }
 
