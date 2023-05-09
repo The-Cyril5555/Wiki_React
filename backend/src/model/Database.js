f121aab9a296866d28b046a027b49b0b76df4288
Cyril Bizouarn <cyrilbizouarn5@gmail.com>
2023-05-09 19:12:42 +0200

    Utilisation d'un pool pour les connections mysql

--- backend/src/model/Database.js
+++ backend/src/model/Database.js
@@ -1,21 +1,16 @@
-/**
- * DBConnexion.js
- * Ce fichier gère la connexion à la base de données MySQL contenant les fichiers, dossiers et mots-clés.
- * Si la base de données n'existe pas, elle est automatiquement créée.
- */
-
-// Importation du module MySQL
 const mysql = require('mysql2');
 require('dotenv').config();
 
-// Création de la connexion à la base de données
-const db = mysql.createConnection({
-   'host': process.env.MYSQL_HOST,
-   'user': process.env.MYSQL_USER,
-   'password': process.env.MYSQL_PASSWORD,
-   'database': process.env.MYSQL_DATABASE,
-   'port': process.env.MYSQL_PORT,
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
+  waitForConnections: true,
+  queueLimit: 0
 });
 
-// Exportation de la connexion à la base de données
-module.exports = db;
+// Exportation du pool de connexions
+module.exports = pool;
